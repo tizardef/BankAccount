0a8f6efa488c1a7fdb91031f4c8cd1249642d92b
tizardef <tizardef@g.cofc.edu>
2016-08-23 16:22:19 -0400

    Account Class for Assignment 6

--- BankAccount/src/ssa/Account.java
+++ BankAccount/src/ssa/Account.java
@@ -3,20 +3,33 @@ package ssa;
 public class Account {
 	//properties
 	
-		int accountId;
-		String accountDescription; 
-		double accountBalance;
-		double checking; 
-		double savings;
-
+		private int accountId;
+		String description; 
+		private double accountBalance;
+		
+		//constructor to set the initial account balance to 0
+		public Account() {
+			   this.accountBalance = 0;
+		
+		}
+		public Account(String description) {
+			   this.description = description;
+			   this.accountBalance = 0;
+		}
+		public Account(int accountId, String description) {
+			   this.accountId = accountId;
+			   this.description = description;
+			   this.accountBalance = 0;
+			
+		}
 		//deposit method
 		void deposit(double depositAmount) {
 			 accountBalance = accountBalance + depositAmount;
 		}
 		//withdraw method
-		void withdraw(double withrdrawAmount) {
-			 if ((accountBalance - withrdrawAmount) >= 0) {
-				accountBalance = accountBalance - withrdrawAmount; 
+		void withdraw(double withdrawAmount) {
+			 if ((accountBalance - withdrawAmount) >= 0) {
+				accountBalance = accountBalance - withdrawAmount; 
 			 }
 			 else {
 				 System.out.println("Insufficient funds! ");
@@ -26,18 +39,30 @@ public class Account {
 		
 		void getAccountStatus() {
 			System.out.println("The account ID is: " +  accountId);
-			System.out.println("Account Description: " + accountDescription);
+			System.out.println("Account Description: " + description);
 			System.out.println("The account balance is: " + accountBalance);
 			System.out.println("******************************************************");
 		}
-		void setInitialBalance(double currentAccountBalance) {
-			accountBalance = currentAccountBalance;
-		}
-		void setAccountId(int currentAccountId) {
+		//private void setInitialBalance(double accountBalance) {
+			//this.accountBalance = accountBalance;
+		
+	
+		
+		
+		private void setAccountId(int currentAccountId) {
 			accountId = currentAccountId;
 			
 		}
-		void setAccountDescription(String currentAccountDescription) {
-			accountDescription = currentAccountDescription;
+		public int getAccountId() {
+			return accountId;
+		}
+		
+		void setAccountDescription(String description) {
+			this.description = description;
+		}
+		
+		public void transferFrom(Account account, double amount) {
+			
+			
 		}
 }
